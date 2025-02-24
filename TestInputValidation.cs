0c5b2249a53db9c3854039e776ce6f6cb5e3e955
Lily Smith <81171629+lilysmith1206@users.noreply.github.com>
2025-02-24 17:45:01 -0500

    Create initial files

---
+++ TestInputValidation.cs
@@ -0,0 +1,16 @@
+// Tests/TestInputValidation.cs
+using NUnit.Framework;
+[TestFixture]
+public class TestInputValidation
+{
+	[Test]
+	public void TestForSQLInjection()
+	{
+		// Placeholder for SQL Injection test
+	}
+	[Test]
+	public void TestForXSS()
+	{
+		// Placeholder for XSS test
+	}
+}
